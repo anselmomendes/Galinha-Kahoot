bc35e0527c25fb00ed32144370de94bfe0e9dc52
Alex <alexwendel11@gmail.com>
2020-04-22 20:17:02 -0300

    Nova tela de cadastro

--- lib/app/modules/student/pages/student_cadastro/student_cadastro_page.dart
+++ lib/app/modules/student/pages/student_cadastro/student_cadastro_page.dart
@@ -1,24 +1,126 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:galinha_karoot/app/modules/common/styles.dart';
 
-class StudentCadastroPage extends StatefulWidget {
-  final String title;
-  const StudentCadastroPage({Key key, this.title = "StudentCadastro"})
+
+class StudentCadastroPage extends StatefulWidget{
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    final String title;
+
+    StudentCadastroPage({Key key, this.title = "Novo cadastro"})
       : super(key: key);
 
-  @override
-  _StudentCadastroPageState createState() => _StudentCadastroPageState();
+    @override
+    _StudentCadastroPageState createState() => _StudentCadastroPageState();
+
 }
 
 class _StudentCadastroPageState extends State<StudentCadastroPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: <Widget>[],
-      ),
-    );
-  }
+
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+    final TextEditingController _emailController = TextEditingController();
+    final TextEditingController _passwordController = TextEditingController();
+    final TextEditingController _universityController = TextEditingController();
+    final TextEditingController _nameController = TextEditingController();
+    bool _success;
+    String _userEmail;
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+          body: _body()       
+        );
+    }
+
+    _body(){      
+      var screenWidth = MediaQuery.of(context).size.width;
+
+
+      return SingleChildScrollView(
+          padding: EdgeInsets.only(top: screenWidth * 0.2, left: screenWidth * 0.05, right: screenWidth * 0.05),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Image.asset("assets/bits.png", width: MediaQuery.of(context).size.width * appLogoMediumSize),
+                SizedBox(height: 20),
+                Text("Registro de aluno", style: headerTextStyle),
+                SizedBox(height: 20),
+
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+
+                      // Nome
+                      TextFormField(
+                        controller: _nameController,
+                        decoration: const InputDecoration(
+                          labelText: 'Nome', 
+                          hintText: "Nome completo",
+                          prefixIcon:Icon(Icons.person),               
+                        )
+                      ),
+
+                      // Universidade
+                      TextFormField(
+                        controller: _nameController,
+                        decoration: const InputDecoration(
+                          labelText: 'Universidade', 
+                          hintText: "Nome da Universidade",
+                          prefixIcon:Icon(Icons.school),               
+                        )
+                      ),
+
+                      // Email
+                      TextFormField(
+                        controller: _nameController,
+                        decoration: const InputDecoration(
+                          labelText: 'Email', 
+                          hintText: "Endereço de email",
+                          prefixIcon:Icon(Icons.mail),               
+                        )
+                      ),                      
+
+                      // Senha
+                      TextFormField(
+                        controller: _nameController,
+                        decoration: const InputDecoration(
+                          labelText: 'Senha', 
+                          hintText: "Senha",
+                          prefixIcon:Icon(Icons.lock),               
+                        )
+                      ),     
+
+                    ]                    
+                  )
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    FlatButton(onPressed: _register,color: appColor, child: Text('Registrar-se', style: TextStyle(color: Colors.white)))
+                  ]
+                )
+
+              ]
+            )
+          )
+        );
+    }
+
+    @override
+    void dispose() {
+      _emailController.dispose();
+      _passwordController.dispose();
+      super.dispose();
+    }
+
+    void _register() async {
+      final FirebaseUser user = (await widget._auth.createUserWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      )).user;
+      print(user);
+    }
+
 }
