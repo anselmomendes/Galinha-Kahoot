9b5c9462029f479f2c4bb396364f360c5b97d206
Anselmo <anselmo.96@hotmail.com>
2020-04-22 23:07:24 -0300

    Models e repositories

--- lib/app/modules/teacher/services/interfaces/teacher_services_interfaces.dart
+++ lib/app/modules/teacher/services/interfaces/teacher_services_interfaces.dart
@@ -1,6 +1,6 @@
 import 'package:galinha_karoot/app/modules/teacher/models/TeacherModel.dart';
 
-abstract class ITeacherRepository {
+abstract class ITeacherServices {
   Stream<List<TeacherModel>> getAllStream();
   List<TeacherModel> getAll();
   Future save(TeacherModel model);
