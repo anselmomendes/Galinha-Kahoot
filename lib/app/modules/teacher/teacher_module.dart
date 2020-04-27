1fdd229612aa4cdffd147fdab25044734c744a72
unknown <anselmo.96@hotmail.com>
2020-04-27 03:00:17 -0300

    Liberação da rota

--- lib/app/modules/teacher/teacher_module.dart
+++ lib/app/modules/teacher/teacher_module.dart
@@ -28,7 +28,8 @@ class TeacherModule extends ChildModule {
         Bind((i) => TeacherCadastroController()),
         Bind((i) => TeacherLoginController()),
         Bind((i) => TeacherService()),
-        Bind((i) => TeacherRepository()), //firestore: Firestore.instance)),
+        Bind((i) => TeacherRepository(
+            firestore: Firestore.instance)), //firestore: Firestore.instance)),
       ];
 
   @override
@@ -39,7 +40,7 @@ class TeacherModule extends ChildModule {
         Router('/teacher_menu', child: (_, args) => TeacherMenuPage()),
         Router('/teacher_list_cases',
             child: (_, args) => TeacherListCasesPage()),
-        //Router('/teacher_report', child: (_, args) => TeacherReportPage())
+        Router('/teacher_report', child: (_, args) => TeacherReportPage())
       ];
 
   static Inject get to => Inject<TeacherModule>.of();
