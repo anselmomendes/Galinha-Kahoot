80c3d99ff666443aac329f336bb1b4618563e348
fernandofreire01 <fernando.freire.94@hotmail.com>
2020-04-27 00:48:04 -0300

    Ajuste na page teacher_report (teste)

--- lib/app/modules/teacher/pages/teacher_report/teacher_report_page.dart
+++ lib/app/modules/teacher/pages/teacher_report/teacher_report_page.dart
@@ -10,6 +10,9 @@ class TeacherReportPage extends StatefulWidget {
 }
 
 class _TeacherReportPageState extends State<TeacherReportPage> {
+  final List<String> entries = <String>['A', 'B', 'C'];
+  final List<int> colorCodes = <int>[600, 500, 100];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,84 +21,94 @@ class _TeacherReportPageState extends State<TeacherReportPage> {
       ),
       body: Column(
         children: <Widget>[
-          Center(
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 25),
-                Text("Olá professor 'Vegano'! ",
-                    style:
-                        TextStyle(fontSize: 20, fontWeight: FontWeight.w600)),
-                SizedBox(height: 5),
-                Text(
-                    "Gere relatório por turmas. \nEscolhar a turma que deseja gerar o relatório:",
-                    textAlign: TextAlign.center),
-                SizedBox(height: 20),
-              ],
-            ),
-          ),
           Container(
-            child: SingleChildScrollView(
+            child: Center(
               child: Column(
                 children: <Widget>[
-                  Text('data1'),
-                  Text('data1'),
-                  Text('data1'),
-                SizedBox(height: 20),
-                  
-                  Text('data1'),
-                  Text('data1'),
-                  Text('data1'),
-                  Text('data1'),
-                SizedBox(height: 20),
-
-                  Text('data1'),
-                  Text('data1'),
-                  Text('data1'),
-                SizedBox(height: 20),
-
-                  Text('data1'),
-                  Text('data1'),
-                  Text('data1'),
-                  Text('data1'),
-                  Text('data1'),
-                SizedBox(height: 20),
-                  
-                  Text('data1'),
-                  Text('data1'),
-                  Text('data1'),
-                  Text('data1'),
-                SizedBox(height: 20),
-
-                  Text('data1'),
-                  Text('data1'),
-                  Text('data1'),
-                SizedBox(height: 20),
-
-                  Text('data1'),
-                  Text('data1'),
-                  Text('data1'),
-                  Text('data1'),
-                  Text('data1'),
-                SizedBox(height: 20),
-                  
-                  Text('data1'),
-                  Text('data1'),
-                  Text('data1'),
-                  Text('data1'),
-                SizedBox(height: 20),
-
-                  Text('data1'),
-                  Text('data1'),
-                  Text('data1'),
-                SizedBox(height: 20),
-
-                  Text('data1'),
-                  Text('data1'),
-
+                  SizedBox(height: 25),
+                  Text("Olá professor 'Vegano'! ",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w600)),
+                  SizedBox(height: 5),
+                  Text(
+                      "Gere relatório por turmas. \nEscolhar a turma que deseja gerar o relatório:",
+                      textAlign: TextAlign.center),
+                  SizedBox(height: 20),
+                  Center(
+                    child: Container(
+                    margin: EdgeInsets.all(10),
+                    height: 500,
+                    child: ListView(
+                      scrollDirection: Axis.vertical,
+                      children: <Widget>[
+                        RaisedButton(
+                          onPressed: null,
+                          child: Text("Facebook"),
+                        ),
+                        Padding(padding: EdgeInsets.all(5.00)),
+                        RaisedButton(
+                          onPressed: null,
+                          child: Text("Google"),
+                        ),
+                        SizedBox(height: 25),
+                        Text("Olá professor 'Vegano'! ",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w600)),
+                        SizedBox(height: 5),
+                        Text(
+                            "Gere relatório por turmas. \nEscolhar a turma que deseja gerar o relatório:",
+                            textAlign: TextAlign.center),
+                        SizedBox(height: 20),
+                        RaisedButton(
+                          onPressed: null,
+                          child: Text("Facebook"),
+                        ),
+                        Padding(padding: EdgeInsets.all(5.00)),
+                        RaisedButton(
+                          onPressed: null,
+                          child: Text("Google"),
+                        ),
+                        SizedBox(height: 25),
+                        Text("Olá professor 'Vegano'! ",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w600)),
+                        SizedBox(height: 5),
+                        Text(
+                            "Gere relatório por turmas. \nEscolhar a turma que deseja gerar o relatório:",
+                            textAlign: TextAlign.center),
+                        SizedBox(height: 20),
+                        Text('data1'),
+                        Text('data1'),
+                        Text('data1'),
+                        SizedBox(height: 20),
+                        Text('data1'),
+                        Text('data1'),
+                        Text('data1'),
+                        Text('data1'),
+                        SizedBox(height: 20),
+                        Text('data1'),
+                        Text('data1'),
+                        Text('data1'),
+                        SizedBox(height: 20),
+                        Text('data1'),
+                        Text('data1'),
+                        Text('data1'),
+                        Text('data1'),
+                        Text('data1'),
+                        SizedBox(height: 20),
+                        Text('data1'),
+                        Text('data1'),
+                        Text('data1'),
+                        Text('data1'),
+                        SizedBox(height: 20),
+                      ],
+                    ),
+                  ),
+                  )
                 ],
               ),
             ),
-          )
+          ),
         ],
       ),
     );
