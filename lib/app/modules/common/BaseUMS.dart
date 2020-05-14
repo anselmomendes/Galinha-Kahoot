3bb073959cbf66c219fc706da129b1533cd5d3fa
Alex <alexwendel11@gmail.com>
2020-05-14 16:21:19 -0300

    Novo arquivo

---
+++ lib/app/modules/common/BaseUMS.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'BaseAuth.dart';
+
+abstract class BaseUMS {
+
+    // Future<Map> getClass(String classId);
+    // Future<Map> getClassesList(String uid = null, int limit = 0);    
+    // Future<Map> getCase(String caseId);
+    // Future<Map> getCasesList(String uid = null, int limit = 0);
+    // Future<Map> getNotificationsList(String uid = null, int limit = 0);
+    // Future<int> getCaseAnswerScore(String caseId);
+
+}
+
+class KAuth extends Auth{	
+
+
+
+	// Future<Map> getUserClasses(){
+	// 	Firestore.instance.collection("classes")
+	// }
+
+}
