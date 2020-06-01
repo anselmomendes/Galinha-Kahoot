aeaa011f1775d2357fd6224c9492146186cbf75c
Alex <alexwendel11@gmail.com>
2020-06-01 08:26:03 -0300

    Created Image Model

---
+++ lib/app/modules/common/models/ImageModel.dart
@@ -0,0 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ImageModel {
+	int id;
+	String md5; 
+	String relatedTo;
+	DateTime createdAt;
+
+	ImageModel({
+		this.id,
+		this.md5,
+		this.relatedTo,
+		this.createdAt
+	});
+
+	factory ImageModel.fromDocument(DocumentSnapshot doc) => ImageModel(
+		id: doc["id"],
+		md5: doc["md5"],
+		relatedTo: doc["relatedTo"],
+		createdAt: doc["createdAt"]
+	);
+
+	Map<String, dynamic> toJSON() => {
+		"id": id,
+		"md5": md5,
+		"relatedTo": relatedTo,
+		"createdAt": createdAt
+	};
+
+}
