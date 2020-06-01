228aa9fd192569d3c2b344be41ad0a9a918e2920
Alex <alexwendel11@gmail.com>
2020-06-01 19:09:47 -0300

    Getter da propriedade filename adicionado

--- lib/app/modules/common/models/ImageModel.dart
+++ lib/app/modules/common/models/ImageModel.dart
@@ -1,22 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/widgets.dart';
 
 class ImageModel {
 	int id;
 	String md5; 
 	String relatedTo;
+  String format;
 	DateTime createdAt;
 
 	ImageModel({
 		this.id,
 		this.md5,
+    this.format,
 		this.relatedTo,
 		this.createdAt
 	});
 
+  get filename {
+      return "$md5.$format";
+  }
+
 	factory ImageModel.fromDocument(DocumentSnapshot doc) => ImageModel(
 		id: doc["id"],
 		md5: doc["md5"],
+    format: doc["format"],
 		relatedTo: doc["relatedTo"],
 		createdAt: doc["createdAt"]
 	);
@@ -24,8 +30,10 @@ class ImageModel {
 	Map<String, dynamic> toJSON() => {
 		"id": id,
 		"md5": md5,
+    "format": format,
 		"relatedTo": relatedTo,
-		"createdAt": createdAt
+		"createdAt": createdAt,
+    "filename": filename
 	};
 
 }
