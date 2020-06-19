6e2cd54874edeee28303d13a52a73a87cb42e721
Hatake Attack <fernando.freire.94@hotmail.com>
2020-06-19 01:41:59 -0300

    Ajustes função cadastro conta professor e tratamento de parada signIn
    Add um simples tratamento

--- lib/app/modules/common/BaseAuth.dart
+++ lib/app/modules/common/BaseAuth.dart
@@ -34,8 +34,10 @@ class Auth implements BaseAuth {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   Future<String> signIn(String email, String password) async {
-    FirebaseUser user = (await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password)).user;
     try {
+      FirebaseUser user = (await _firebaseAuth.signInWithEmailAndPassword(
+              email: email, password: password))
+          .user;
       return user.uid;
     } catch (e) {
       print("Erro de signIn: $e");
@@ -45,7 +47,8 @@ class Auth implements BaseAuth {
 
   Future<String> signUp(String email, String password) async {
     FirebaseUser user = (await _firebaseAuth.createUserWithEmailAndPassword(
-        email: email, password: password)).user;
+            email: email, password: password))
+        .user;
     return user.uid;
   }
 
@@ -102,68 +105,75 @@ class Auth implements BaseAuth {
   }
 
   @override
-  Future<void> sendPasswordResetMail(String email) async{
+  Future<void> sendPasswordResetMail(String email) async {
     await _firebaseAuth.sendPasswordResetEmail(email: email);
     return null;
   }
-  
-  
 
-	List metaFields = ["uid", "role", "name", "university"];
+  List metaFields = ["uid", "role", "name", "university"];
 
-  Future<bool> createUserMeta(String role, String name, String university) async {
+  Future<bool> createUserMeta(
+      String role, String name, String university) async {
+    FirebaseUser currentUser = await getCurrentUser();
+    if (currentUser == null)
+      throw Exception("Access denied: No user currently signed in.");
+    return Firestore.instance
+        .collection("users")
+        .where("uid", isEqualTo: currentUser.uid)
+        .getDocuments()
+        .then((docs) {
+      print(docs);
+      if (docs.documents.isEmpty) {
+        Firestore.instance
+            .collection("users")
+            .document(currentUser.uid)
+            .setData({
+          "uid": currentUser.uid,
+          "role": role,
+          "name": name,
+          "university": university
+        });
 
-		FirebaseUser currentUser = await getCurrentUser();
-		if (currentUser == null)
-			throw Exception("Access denied: No user currently signed in.");
-      return Firestore.instance.collection("users")
-        	.where("uid", isEqualTo: currentUser.uid)
-        	.getDocuments()
-        	.then((docs){
-                print(docs);
-                if (docs.documents.isEmpty){
-                  Firestore.instance.collection("users").document(currentUser.uid).setData({
-                    "uid": currentUser.uid, "role": role, "name": name, "university": university
-                  });
-                  
-                  // add({"uid": currentUser.uid, "role": role, "name": name, "university": university});
-                  return true;
-                }
-				        return false;
-			    });
+        // add({"uid": currentUser.uid, "role": role, "name": name, "university": university});
+        return true;
+      }
+      return false;
+    });
   }
 
-	Future<Map> getUserMeta(){
-		Future<FirebaseUser> currentUser = getCurrentUser();		
-		var docs = Firestore.instance.collection("users")
-					.where("uid", isEqualTo: currentUser)
-					.getDocuments();
+  Future<Map> getUserMeta() {
+    Future<FirebaseUser> currentUser = getCurrentUser();
+    var docs = Firestore.instance
+        .collection("users")
+        .where("uid", isEqualTo: currentUser)
+        .getDocuments();
 
-		docs.then((value){
-			if(value.documents.isEmpty)
-				throw Exception("No meta found for current User. Is it really registered?");
-			else {
-				Map meta;
-				metaFields.forEach((element){
-					meta[element] = value.documents[0][element];
-				});
-				return meta;
-			}
-		});	
-		return null;
-	}
+    docs.then((value) {
+      if (value.documents.isEmpty)
+        throw Exception(
+            "No meta found for current User. Is it really registered?");
+      else {
+        Map meta;
+        metaFields.forEach((element) {
+          meta[element] = value.documents[0][element];
+        });
+        return meta;
+      }
+    });
+    return null;
+  }
 
-	Future<String> getUserRole() async {
-		await getCurrentUser().then((user){
-			Firestore.instance.collection("users")
-			.where("uid", isEqualTo: user.uid)
-			.getDocuments().then((docs){
-				if(docs.documents[0].exists)
-					return docs.documents[0].data['role'];
-				if(docs.documents.isEmpty)
-					return false;
-			});
-		}); 
-		return null;}
-
+  Future<String> getUserRole() async {
+    await getCurrentUser().then((user) {
+      Firestore.instance
+          .collection("users")
+          .where("uid", isEqualTo: user.uid)
+          .getDocuments()
+          .then((docs) {
+        if (docs.documents[0].exists) return docs.documents[0].data['role'];
+        if (docs.documents.isEmpty) return false;
+      });
+    });
+    return null;
+  }
 }
