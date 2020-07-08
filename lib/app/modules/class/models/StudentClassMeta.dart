03a09603c36d6460770b062496737c0041e44685
Alex <alexwendel11@gmail.com>
2020-07-08 19:33:51 -0300

    Propriedade thumbnail adicionada no modelo

--- lib/app/modules/class/models/StudentClassMeta.dart
+++ lib/app/modules/class/models/StudentClassMeta.dart
@@ -7,44 +7,52 @@ class StudentClassMeta {
     String id;
     String name = 'Default class name';
     String description = 'Default class description';
+    String thumbnail = 'thumbnailURL';
     String subject = 'Default class subject';  // May be Reference in the Future
     String inviteCode = 'DefaultClassSubject123';   
 
     Timestamp createdAt = Timestamp.now();
 
-	DocumentReference teacher;
+    DocumentReference teacher;
     DocumentReference reference;
 
     StudentClassMeta({
         this.id,
         this.name,
-		this.description,
-		this.subject,        
+        this.description,
+        this.subject,        
         this.inviteCode,
-		this.createdAt,
-		this.teacher,
-		this.public
+        this.createdAt,
+        this.thumbnail,
+        this.teacher,
+        this.public
     });
 
   	factory StudentClassMeta.fromDocument(DocumentSnapshot doc) => StudentClassMeta(
-        id: doc["id"],
-		name: doc['name'],
-		description: doc['description'],
-		inviteCode: doc['invitation_code'],
-		public: doc['public'],
-		subject: doc['subject'],
-		teacher: doc['teacher'],
-		createdAt: doc['created_at']
+        id: doc.documentID,
+        name: doc['name'],
+        description: doc['description'],
+        inviteCode: doc['invitation_code'],
+        public: doc['public'],
+        subject: doc['subject'],
+        teacher: doc['teacher'],
+        thumbnail: doc['thumbnail'],
+        createdAt: doc['created_at']
       );
 
-	Map<String, dynamic> toJson() => {
-		"id": id,
-		"name": name,
-		"description": description,
-		"subject": subject,
-		"public": public,
-		"inviteCode": inviteCode,
-		"createAt": createdAt,
-		"teacher": teacher.path
-	};
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "description": description,
+        "thumbnail": thumbnail,
+        "subject": subject,
+        "public": public,
+        "inviteCode": inviteCode,
+        "createdAt": createdAt,
+        "teacher": teacher.path
+    };
+
+    toString(){
+      return toJson().toString();	
+    }
 }
