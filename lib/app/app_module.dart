264d6a651ba22ec47d2458b42435a7f0ad1be7c6
lucasvinicius73 <lucasidelfonso73@gmail.com>
2020-11-17 14:16:41 -0300

    case module removido

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -13,14 +13,11 @@ import 'package:galinha_karoot/app/app_widget.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
 import 'package:galinha_karoot/app/modules/welcome/welcome_module.dart';
-import 'modules/cases/pages/navigator_bar/cases_sintomas/cases_sintomas_controller.dart';
 import 'modules/cases/repositories/cases_repository.dart';
 import 'modules/cases/repositories/question_repository.dart';
 import 'modules/cases/store/cases_store.dart';
 import 'modules/cases/view_model/cases_viewmodel.dart';
 import 'modules/users/student_2/student_2_module.dart';
-import 'modules/users/student_2/student_cases/cases_module.dart';
-import 'modules/users/teacher/pages/teacher_list_cases/teacher_list_cases_controller.dart';
 
 class AppModule extends MainModule {
   @override
@@ -42,8 +39,8 @@ class AppModule extends MainModule {
         Router('/student', module: StudentModule()),
         Router('/student_2', module: Student2Module()),
         Router('/teacher', module: TeacherModule()),
-         Router('/student_class', module: StudentClassModule()),
-       Router('/student_cases', module: StudentCasesModule()),
+        Router('/student_class', module: StudentClassModule()),
+        Router('/student_cases', module: StudentCaseModule()),
       ];
 
   @override
